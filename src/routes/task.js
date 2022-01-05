a34d7d9500a09287f42b798d2046c597c3935cd6
Assaf Sabag <assafsabag@h-MacBook-Pro-sl-Assaf.local>
2022-01-05 23:59:40 +0200

    add routes

---
+++ src/routes/task.js
@@ -0,0 +1,130 @@
+const express = require('express')
+const Task = require('../models/task.js')
+const taskroute = new express.Router()
+const auth = require('../middleware/Authorization.js');
+const User = require('../models/user.js')
+
+taskroute.post('/task', auth, async (req,res)=>{
+    const task=  new Task({
+        ...req.body,
+        owner: req.user._id
+      
+
+    })
+       
+    try{
+        await task.save()
+        console.log(req.user)
+        res.status(200).send('Success!'+ task)
+    }
+    catch(e){
+        res.status(400).send('Error!' + e)
+    }
+})
+
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
+taskroute.get('/tasks',auth, async (req,res)=>{
+    
+         try{ 
+             //const tasks= await Task.find({owner : req.user._id}) 
+       const match={}
+       const sort ={}
+        if (req.query.completed){
+          match.completed = req.query.completed==='true'
+        }
+        
+        if (req.query.sort){
+        const partss= req.query.sort.split(':')
+
+        sort[partss[0]]= sort[partss[1]]==='desc' ? 1: -1
+        }
+       await req.user.populate({
+            
+            path: 'tasks', 
+            match: match , 
+            options:{
+                limit: parseInt(req.query.limit),
+                skip:parseInt(req.query.skip),
+                sort
+            }
+        
+        
+        })
+        console.log(sort)
+       return res.send(req.user.tasks)
+    }
+    catch(e){
+        res.status(400).send('Error!' +e )
+    }
+    })
+
+
+
+
+    
+taskroute.get('/task/:id', auth ,  (async(req,res)=>{
+    const id = req.params.id
+    try{
+        const tasks= await Task.findOne({_id:id , owner : req.user._id })
+        if(tasks){
+          return res.send(tasks)
+        }
+        res.send('couldnt find tasks that have created by you')
+    }
+    catch(e){
+        res.status(400).send('Error!' +e )
+    }
+}))
+
+taskroute.patch('/task/:id', auth, async (req,res)=>{
+
+    const arr1 = Object.keys(req.body)
+    const arr2= ['description', 'completed']
+
+
+
+
+    const valid = arr1.every((arr1params)=> arr2.includes(arr1params))
+
+    //check if all params in arr 1 be in arr 2
+    if(!valid) {
+        return res.status(400).send( 'Invalid updates!')
+    }
+
+    try{
+        const task = await Task.findOne({_id: req.params.id ,owner : req.user._id })
+        
+        if (!task) {
+            return res.status(404).send('cant find a task to change')
+        }
+        arr1.forEach((arr1params)=>{task[arr1params]=req.body[arr1params]})
+        
+        task.save()
+      
+
+        res.send(task)
+    }
+    catch(e){
+        res.status(400).send('Error!' +e )
+    }
+})
+
+taskroute.delete('/task/:id',auth, async(req,res)=>{
+
+try{
+ const task= await Task.findOneAndDelete({_id: req.params.id, owner:req.user._id})
+ if (!task) {
+    return res.status(404).send('cant find and delete task by this id')
+}
+
+res.status(200).send('deleted'+task)
+}
+catch(e){
+    return  res.status(500).send('e'+e)
+}
+
+
+})
+module.exports= taskroute;
---
+++ src/routes/user.js
@@ -0,0 +1,200 @@
+const User = require('../models/user.js')
+const express = require('express')
+const Task = require('../models/task.js')
+userrouter = new express.Router()
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const auth = require('../middleware/Authorization.js');
+const multer = require('multer')
+const sharp = require('sharp')
+const email = require('../emails/send.js')
+
+
+
+
+
+
+
+
+userrouter.post('/user', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        const jwsent = await user.jwtparser()
+        //   email.welcome(user.name, user.email)
+        res.status(201).send('Success!' + user + '' + jwsent)
+    }
+    catch (e) {
+        res.status(400).send('Error!' + e)
+    }
+})
+
+userrouter.post('/user/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        if (!user) {
+            throw new Error('Unable to login')
+        }
+        
+         const isMatch = await bcrypt.compare( req.body.password , user.password)
+
+        if (!isMatch) {
+            throw new Error('Unable to login')
+        }
+
+        const token = await user.jwtparser()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send('err')
+    }
+})
+
+userrouter.post('/user/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((toke) => {
+
+            if (toke.toke === req.auth) {
+                return false
+            }
+            else { return true }
+
+        }
+        )
+        req.user.save();
+
+        return res.send(req.user)
+    }
+    catch (e) {
+        res.status(400).send('Error!' + e)
+
+    }
+})
+
+userrouter.delete('/user/me', auth, async (req, res) => {
+
+    try {
+        //await req.user.deleteOne({ _id: req.user._id });
+        req.user.remove()
+        email.goodbye(req.user.name, req.user.email)
+        res.status(200).send('deleted' + req.user)
+    }
+    catch (e) {
+        return res.status(500).send('e' + e)
+    }
+
+
+})
+userrouter.post('/user/logoutall', auth, async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+
+        user.tokens = []
+        user.save();
+        return res.send(user)
+    }
+    catch (e) {
+        res.status(400).send('Error!' + e)
+
+    }
+})
+
+
+
+// userrouter.post('/user/login', async (req,res)=>{
+
+
+//     try{
+//         const user = await User.findOne({email:req.body.email })
+//         if(!user) 
+//         return res.status(400).send('cant find this user')
+//             const jwsent =await user.jwtparser()
+//            return res.status(201).send({user , 'token': jwsent})
+
+
+//     }
+
+//     catch(e){
+//         res.status(400).send('Error!' + e)
+
+//     }
+
+// })
+
+
+userrouter.get('/user/:me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+userrouter.patch('/user/me', auth, async (req, res) => {
+
+
+    const arr1 = Object.keys(req.body)
+    const arr2 = ['name', 'email', 'password']
+    const valid = arr1.every((arr1params) => arr2.includes(arr1params))
+    if (!valid) {
+        return res.status(400).send('Invalid updates!')
+
+    }
+
+    try {
+        arr1.forEach((arr1params) => { req.user[arr1params] = req.body[arr1params] })
+
+        req.user.save()
+
+        res.send(req.user)
+
+    }
+    catch (e) {
+        res.status(400).send('Error!' + e)
+    }
+})
+
+const upload = multer({
+    limits: {
+        fileSize: 5000000
+    },
+    fileFilter(req, file, cb) {
+        if (file.originalname.match(/\.(JPG|JPEG|PNG)$/)) {
+            return cb(undefined, true)
+        }
+        cb(new Error('upload only img'))
+    }
+})
+
+userrouter.post('/user/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = req.file.buffer.sharp().resize({ width: 250, height: 250 }).toFile('png').toBuffer()
+    req.user.profile = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+
+    res.status(500).send({ error: error.message })
+})
+
+
+userrouter.delete('/user/me/delete/avatar', auth, async (req, res) => {
+    req.user.profile = undefined
+    await req.user.save()
+    res.send('deleted')
+})
+
+userrouter.get('/user/me/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user || !user.profile) {
+            throw new Error('cant find user e this id')
+        }
+        res.set('Content-Type', 'image/png')
+
+        return res.send(user.profile)
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
+
+
+})
+
+
+module.exports = userrouter;
